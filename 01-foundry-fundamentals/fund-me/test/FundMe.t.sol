572749fcb1ea40554b371961ed9c97a814045efe
Jonny Vango <jonathan.vanessen@gmail.com>
2025-05-03 18:50:20 -0400

    add test

--- 01-foundry-fundamentals/fund-me/test/FundMe.t.sol
+++ 01-foundry-fundamentals/fund-me/test/FundMe.t.sol
@@ -2,7 +2,16 @@
 pragma solidity ^0.8.29;
 
 import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    function setUp() external {}
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUsdIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
 }
