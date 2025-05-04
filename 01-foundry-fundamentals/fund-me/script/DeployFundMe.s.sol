d9c2f777eeeb39c91663a21a5fc28504edda7d9b
Jonny Vango <jonathan.vanessen@gmail.com>
2025-05-04 10:21:47 -0400

    finish dynamic deploy setup

--- 01-foundry-fundamentals/fund-me/script/DeployFundMe.s.sol
+++ 01-foundry-fundamentals/fund-me/script/DeployFundMe.s.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.29;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfigScript} from "./HelperConfig.s.sol";
 
 contract DeployFundMeScript is Script {
     function run() external returns (FundMe) {
+        // Before startBroadcast() => Not a "real" tx
+        HelperConfigScript helperConfig = new HelperConfigScript();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        // After startBroadcast() => "Real" tx
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
--- 01-foundry-fundamentals/fund-me/script/HelperConfig.s.sol
+++ 01-foundry-fundamentals/fund-me/script/HelperConfig.s.sol
@@ -2,5 +2,47 @@
 pragma solidity ^0.8.29;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
 
-contract HelperConfigScript is Script {}
+contract HelperConfigScript is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 1800e8;
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        } else {
+            activeNetworkConfig = getAnvilConfig();
+        }
+    }
+
+    function getSepoliaConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
